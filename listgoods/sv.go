23846d50b9d6c243ee6a45a0ecab2f308186b9a8
guou <guoyu2@zetyun.com>
2022-03-15 09:58:32 +0800

    modify

--- listgoods/sv.go
+++ listgoods/sv.go
@@ -18,7 +18,7 @@ type Good struct {
 
 func (s Good) ListGoods(t string) []Good {
 	ss := []Good{
-		{},
+		{GoodId: 1, GoodName: "内野长绒棉", MinPrice: 12.8, MaxPrice: 16.8, Pic: `neiye/neiye长绒棉/1.jpg`},
 	}
 	return ss
 }
